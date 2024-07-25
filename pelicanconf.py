aa31eaf0f362ea4ed3df4e7702a39482ed2a5bfc
Jan H. Krüger <janhkrueger@outlook.com>
2024-07-26 01:05:50 +0200

    Adjusting SITEURL

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 # Generelle Blogeinstellungen
 AUTHOR = u'Jan H. Krueger'
 SITENAME = u'JHK Blog'
-SITEURL = 'https://janhkrueger.github.io/blog/'
+SITEURL = 'https://janhkrueger.github.io/'
 SITESUBTITLE = 'Das private Blog von Jan H. Krueger'
 TIMEZONE = 'Europe/Berlin'
 
